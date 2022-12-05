1973f62f1efa3911c6b4563954b38a519877fb5a
jeffrey.douglas <jeffrey.douglas@sbevision.com>
2022-12-05 14:20:31 -0500

    Outline ant asserts and reversions

---
+++ test-utils/ants/ants-asserts.js
@@ -0,0 +1,6 @@
+// assert name,symbol, owner
+// assert part inventory
+// assert rarity prices
+// assert is discount used
+// assert price
+// assert ant
---
+++ test-utils/ants/ants-reversion.js
@@ -0,0 +1,6 @@
+// revert with illegal dna: ilegal section index, illegal part index, conflicting parts, unmatched pair part, sold out part
+// get rarity prices bad discount index
+// bad buy skimp discount and not
+// bad discount buy discount already used, not coin owner, both
+// non owner callling owner function
+// get discount dna price color out of bounds
