08242c819f872d3c0110dc99263b37a0ddc1161e
jeffrey.douglas <jeffrey.douglas@sbevision.com>
2022-12-05 14:55:29 -0500

    Outline ant utils
    Added 1 function:
    incrementAntDna:
      -from the given last ant dna find the next ant dna
      -can find next legal or illegal iteration based on input

--- test-utils/ants/ants-utils.js
+++ test-utils/ants/ants-utils.js
@@ -14,10 +14,10 @@ partRarities[11] = [4, 0];
 partRarities[12] = [0, 4, 2, 3, 4, 3, 4, 3, 3, 3, 4];
 partRarities[13] = [0, 3, 4, 3, 3, 3, 4, 3, 4, 3, 3, 3, 3, 3, 3, 4];
 partRarities[14] = [4, 3, 0];
-const maxIndexByRarity = [10000, 3000, 1000, 300, 80];
-const priceScaleByRarity = [1, 2, 3, 8, 15];
+const maxIndexByRarity = [10000, 3000, 1000, 300, 80, 3];
+const priceScaleByRarity = [1, 2, 3, 8, 15, 2500];
 
-const antBuilder = (_isLegal, _isOriginal, _lastAntDna = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 10, 15, 2]) => {
+const _incrementAntDna = (_lastAntDna) => {
     for (let i = 14; i > 0; i--) {
         if (_lastAntDna[i] < partRarities[i].length - 1) {
             _lastAntDna[i]++;
@@ -25,52 +25,53 @@ const antBuilder = (_isLegal, _isOriginal, _lastAntDna = [0, 0, 0, 0, 0, 0, 0, 0
         }
         else {
             _lastAntDna[i] = 0;
+            if (i === 1) {
+                _lastAntDna[0]++
+            }
         }
-        if (i === 0) {
-            if (_isLegal) {
-                if ((_lastAntDna[9] === 0 && _lastAntDna[0] !== 2) || (_lastAntDna[0] === 2 && _lastAntDna[9] !== 0)) {
-                    i = 15;
+    }
+    return _lastAntDna;
+}
+
+const incrementAntDna = (_isLegal, _lastAntDna) => {
+    let isComplete = false;
+    while (!isComplete) {
+        _lastAntDna = _incrementAntDna(_lastAntDna);
+        if (_isLegal) {
+            if (_lastAntDna[3] == 2){
+                if (_lastAntDna[0] !== 2 && _lastAntDna[1] === 0 && _lastAntDna[2] === 0 && _lastAntDna[4] === 0) {
+                    isComplete = true;
                 }
-                if (_lastAntDna[3] === 2 && _lastAntDna[0] === 2) {
-                    i = 15;
+            }
+            if (_lastAntDna[0] === 2 || _lastAntDna[9] === 0) {
+                if (_lastAntDna[0] === 2 && _lastAntDna[9] === 0) {
+                    // do nothing, if already true stay true same for false
+                } else {
+                    isComplete = false;
                 }
-                if (_lastAntDna[3] === 2 && _lastAntDna[1] !== 0) {
-                    i = 15;
+            }
+        } else {
+            if (_lastAntDna[3] == 2){
+                if (_lastAntDna[0] === 2 || _lastAntDna[1] !== 0 || _lastAntDna[2] !== 0 || _lastAntDna[4] !== 0) {
+                    isComplete = true;
                 }
-                if (_lastAntDna[3] === 2 && _lastAntDna[2] !== 0) {
-                    i = 15;
-                }
-                if (_lastAntDna[3] === 2 && _lastAntDna[4] !== 0) {
-                    i = 15;
-                }
-            } else {
-                if ((_lastAntDna[9] === 0 && _lastAntDna[0] !== 2) || (_lastAntDna[0] === 2 && _lastAntDna[9] !== 0)) {
-                    i = 15;
-                }
-                if (_lastAntDna[3] === 2 && _lastAntDna[0] === 2) {
-                    i = 15;
-                }
-                if (_lastAntDna[3] === 2 && _lastAntDna[1] !== 0) {
-                    i = 15;
-                }
-                if (_lastAntDna[3] === 2 && _lastAntDna[2] !== 0) {
-                    i = 15;
-                }
-                if (_lastAntDna[3] === 2 && _lastAntDna[4] !== 0) {
-                    i = 15;
+            }
+            if (_lastAntDna[0] === 2 || _lastAntDna[9] === 0) {
+                if (_lastAntDna[0] !== 2 || _lastAntDna[9] !== 0) {
+                   isComplete = true;
                 }
             }
         }
     }
-    if (_dna[3] == 2) {
-        require(_dna[0] != 2, "EOD and gas masks incompatible!");
-        require(_dna[1] == 0, "Gas mask incompatible with optical!");
-        require(_dna[2] == 0, "Gas mask incompatible with mouth!");
-        require(_dna[4] == 0, "Gas mask incompatible with face!");
-
-    }
-    if (_dna[0] == 2 || _dna[9] == 0) {
-        require(_dna[0] == 2, "EOD must be paired!");
-        require(_dna[9] == 0, "EOD must be paired!");
-    }
+    return _lastAntDna;
 }
+
+// update base uri
+// update max stock
+// update price scale
+// update min fee
+// update part rarity
+// update coin address
+// create ant
+// create X ants
+// create promotional ant
