164195e4ac77ee9fbbf79e27f4700a6129577825
jeffrey.douglas <jeffrey.douglas@sbevision.com>
2022-12-05 11:41:07 -0500

    Add ants contract:
    -inherits:
      ~AntParts,
      ~ERC721,
      ~Ownable
    
    -usage:
      ~create ant nfts with given dna
      ~discount ant purchases based on coin ownership(1 time per coin)
      ~updates(only contract owner):
        +baseURI
        +max stock of attribute by rarity
        +price scale by rarity(minFee*scale)=partprice
        +min fee
        +part rarity or the rarity of a specific attribute
        +address of coin contract
      ~check if coin's discount is used
      ~gets:
        +discounted price of given dna
        +regular price of given dna
        +Ant in the form of dna array[15] and rarity array[15]
    
    Add AntParts contract for Ants to inherit:
    
      -data handled by AntParts:
        ~each part/attribute rarity
        ~count for each part/attribute
        ~the max stock/supply fo reach rarity
        ~the price scale for each rarity
        ~the minimum part fee
    
      -usage by ants contract:
        ~all updates seen in ants contract excluding:
          --uri,
          --coin contract address
        ~gets:
          +array[15] of rarities based on given dna array[15]
          +price of given dna array[15] while requiring dna validity
        ~increments part counts based on given dna array
    
      -external usage:
        ~gets:
          +remaining inventory of specified part
          +prices of each rarity based on given discount

---
+++ contracts/AntParts.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract AntParts {
+
+    uint8[][15] partRarities;
+    uint16[][15] partCounts;
+    uint16[6] maxStockByRarity;
+    uint16[6] priceScaleByRarity;
+    uint64 minPartFee = .0009 ether;
+
+    constructor() {
+        partRarities[0] = [0, 3, 4, 1, 3, 3, 4, 1, 1, 2, 2, 3, 2, 2, 2];
+        partCounts[0] = new uint8[](15);
+        partRarities[1] = [0, 1, 3, 4, 2];
+        partCounts[1] = new uint8[](5);
+        partRarities[2] = [0, 3, 4];
+        partCounts[2] = new uint8[](3);
+        partRarities[3] = [0, 3, 4];
+        partCounts[3] = new uint8[](3);
+        partRarities[4] = [0, 2, 3];
+        partCounts[4] = new uint8[](3);
+        partRarities[5] = [0, 3];
+        partCounts[5] = new uint8[](2);
+        partRarities[6] = [3, 3, 0];
+        partCounts[6] = new uint8[](3);
+        partRarities[7] = [4, 4, 4, 0, 3, 4, 4, 2, 2, 1, 2, 2];
+        partCounts[7] = new uint8[](12);
+        partRarities[8] = [0, 3, 3, 3];
+        partCounts[8] = new uint8[](4);
+        partRarities[9] = [4, 2, 3, 3, 0, 0, 4, 1, 1, 3, 3, 3, 3];
+        partCounts[9] = new uint8[](13);
+        partRarities[10] = [4, 0];
+        partCounts[10] = new uint8[](2);
+        partRarities[11] = [4, 0];
+        partCounts[11] = new uint8[](2);
+        partRarities[12] = [0, 4, 2, 3, 4, 3, 4, 3, 3, 3, 4];
+        partCounts[12] = new uint8[](11);
+        partRarities[13] = [0, 3, 4, 3, 3, 3, 4, 3, 4, 3, 3, 3, 3, 3, 3, 4];
+        partCounts[13] = new uint8[](16);
+        partRarities[14] = [4, 3, 0];
+        partCounts[14] = new uint8[](3);
+        maxStockByRarity = [10000, 3000, 1000, 300, 80, 3];
+        priceScaleByRarity = [1, 2, 3, 8, 15, 2500];
+    }
+
+    function _updateMaxStock(uint _maxStockIndex, uint16 _newMaxStock) internal {
+        maxStockByRarity[_maxStockIndex] = _newMaxStock;
+    }
+
+    function _updatePriceScale(uint _priceScaleIndex, uint16 _newPriceScale) internal {
+        priceScaleByRarity[_priceScaleIndex] = _newPriceScale;
+    }
+
+    function _updateMinFee(uint64 _minFee) internal {
+        minPartFee = _minFee;
+    }
+
+    function _updatePartRarity(uint _secIndex, uint _partIndex, uint8 _rarity) internal {
+        partRarities[_secIndex][_partIndex] = _rarity;
+    }
+
+    function _getRarities(uint[15] memory _dna) internal view returns (uint[15] memory rarities) {
+        for (uint i; i < 15; ++i) {
+            rarities[i] = partRarities[i][_dna[i]];
+        }
+    }
+
+    function _getDnaPrice(uint[15] memory _dna, uint _discountIndex) internal view returns (uint price) {
+        require(_discountIndex < 6, "Discount index out of bounds!");
+        if (_dna[3] == 2) {
+            require(_dna[0] != 2, "EOD and gas masks incompatible!");
+            require(_dna[1] == 0, "Gas mask incompatible with optical!");
+            require(_dna[2] == 0, "Gas mask incompatible with mouth!");
+            require(_dna[4] == 0, "Gas mask incompatible with face!");
+
+        }
+        if (_dna[0] == 2 || _dna[9] == 0) {
+            require(_dna[0] == 2, "EOD must be paired!");
+            require(_dna[9] == 0, "EOD must be paired!");
+        }
+        for (uint i; i < 15; ++i) {
+            require(_dna[i] < partCounts[i].length, "Invalid DNA!");
+            uint partRarity = partRarities[i][_dna[i]];
+            require((maxStockByRarity[partRarity] - partCounts[i][_dna[i]]) > 0, "Part is sold out!");
+            if (_discountIndex == 0) {
+                price += minPartFee * priceScaleByRarity[partRarity];
+            } else {
+                price += minPartFee * priceScaleByRarity[partRarity] * (100 - 10 * _discountIndex) / 100;
+            }
+        }
+    }
+
+    function _incrementCounts(uint[15] memory _dna) internal {
+        for (uint i; i < 15; ++i) {
+            partCounts[i][_dna[i]] = partCounts[i][_dna[i]] + 1;
+        }
+    }
+
+    function getPartInventory(uint _secIndex, uint _partIndex) external view returns (uint inStock) {
+        uint rarity = partRarities[_secIndex][_partIndex];
+        inStock = maxStockByRarity[rarity] - partCounts[_secIndex][_partIndex];
+    }
+
+    function getRarityPrices(uint _discountIndex) external view returns (uint[6] memory prices) {
+        require(_discountIndex < 6, "Discount index out of bounds!");
+        uint minFee = minPartFee;
+        uint16[6] memory priceScale = priceScaleByRarity;
+        for (uint i; i < 6; ++i) {
+            if (_discountIndex == 0) {
+                prices[i] = priceScale[i] * minFee;
+            } else {
+                prices[i] = priceScale[i] * minFee * (100 - 10 * _discountIndex) / 100;
+            }
+        }
+    }
+}
---
+++ contracts/Ants.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./AntParts.sol";
+
+interface ICoins {
+    function getColor(uint) external view returns (uint);
+    function ownerOf(uint) external view returns (address);
+}
+
+contract Ants is ERC721, Ownable, AntParts {
+    uint16 public COUNTER;
+    address coinAddr;
+    string baseURI = "localhost:3001/ants/";
+
+    constructor(address _coinAddr) ERC721("ArmyAnts", "AA") {
+        coinAddr = _coinAddr;
+    }
+
+    uint104[] public ants;
+
+    mapping(uint16 => bool) isDiscountUsedByID;
+
+    event NewAnt(address indexed owner, uint id);
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function _createDna(uint _rawDna) internal pure returns (uint[15] memory resultDna) {
+        for (uint i; i < 15; ++i) {
+            resultDna[i] = _rawDna / 100 ** (14 - i) % 100;
+        }
+    }
+
+    function _createAnt(uint104 _rawDna) private {
+        ants.push(_rawDna);
+        _safeMint(msg.sender, COUNTER);
+        emit NewAnt(msg.sender, COUNTER);
+        COUNTER++;
+    }
+
+    function withdraw() external payable onlyOwner {
+        address payable _owner = payable(owner());
+        _owner.transfer(address(this).balance);
+    }
+
+    function isDiscountUsed(uint16 _id) external view returns (bool) {
+        return isDiscountUsedByID[_id];
+    }
+
+    function updateBaseURI(string memory _newURI) external onlyOwner {
+        baseURI = _newURI;
+    }
+
+    function updateMaxStock(uint _maxStockIndex, uint16 _newMaxStock) external onlyOwner {
+        _updateMaxStock(_maxStockIndex, _newMaxStock);
+    }
+
+    function updatePriceScale(uint _priceScaleIndex, uint16 _newPriceScale) external onlyOwner {
+        _updatePriceScale(_priceScaleIndex, _newPriceScale);
+    }
+
+    function updateMinFee(uint64 _minFee) external onlyOwner {
+        _updateMinFee(_minFee);
+    }
+
+    function updatePartRarity(uint _secIndex, uint _partIndex, uint8 _rarity) external onlyOwner {
+        _updatePartRarity(_secIndex, _partIndex, _rarity);
+    }
+
+    function updateCoinAddr(address _coinAddr) external onlyOwner {
+        coinAddr = _coinAddr;
+    }
+
+    function createAnt(uint104 _rawDna) external payable {
+        require(COUNTER < 60000, "Ant limit reached!");
+        uint[15] memory dna = _createDna(_rawDna);
+        uint price = _getDnaPrice(dna, 0);
+        require(msg.value >= price, "Not enough ETH!");
+        _createAnt(_rawDna);
+        _incrementCounts(dna);
+    }
+
+    function createDiscountAnt(uint16 _coinId, uint104 _rawDna) external payable {
+        require(COUNTER < 60000, "Ant limit reached!");
+        require(ICoins(coinAddr).ownerOf(_coinId) == msg.sender, "You don't own that coin!");
+        require(!isDiscountUsedByID[_coinId], "Discount already used!");
+        uint[15] memory dna = _createDna(_rawDna);
+        uint color = ICoins(coinAddr).getColor(_coinId);
+        uint price = _getDnaPrice(dna, color + 1);
+        require(msg.value >= price, "Not enough ETH!");
+        _createAnt(_rawDna);
+        _incrementCounts(dna);
+        isDiscountUsedByID[_coinId] = true;
+    }
+
+    function getDiscountDnaPrice(uint _coinColor, uint _rawDna) external view returns (uint price) {
+        require(_coinColor < 5, "Color index out of bounds!");
+        uint[15] memory dna = _createDna(_rawDna);
+        price = _getDnaPrice(dna, _coinColor + 1);
+    }
+
+    function getDnaPrice(uint _rawDna) external view returns (uint price) {
+        uint[15] memory dna = _createDna(_rawDna);
+        price = _getDnaPrice(dna, 0);
+    }
+
+    function getAnt(uint _id) external view returns (uint[15] memory dna, uint[15] memory rarities) {
+        dna = _createDna(ants[_id]);
+        rarities = _getRarities(dna);
+    }
+}
