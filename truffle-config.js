3776e0611e44732c1968a3be9be0874c05122f8c
jeffrey.douglas <jeffrey.douglas@sbevision.com>
2022-12-05 20:21:16 -0500

    Update truffle test config

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,8 @@
 module.exports = {
   networks: {
-    loc_development_development: {
-      network_id: "*",
-      port: 8545,
-      host: "127.0.0.1"
+    test: {
+      total_accounts: 20,
+      default_balance_ether: 10000,
     }
   },
   mocha: {},
