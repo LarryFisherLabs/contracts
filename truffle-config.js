02e47ae2be1960ebe059f953d01bc7f13e3b052a
jeffrey.douglas <jeffrey.douglas@sbevision.com>
2023-01-07 12:15:31 -0500

    add development network for manual testing

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,12 @@ module.exports = {
   networks: {
     test: {
       total_accounts: 20,
-      default_balance_ether: 10000,
+      default_balance_ether: 10000
+    },
+    loc_development_development: {
+      network_id: "*",
+      port: 8545,
+      host: "127.0.0.1"
     }
   },
   mocha: {},
